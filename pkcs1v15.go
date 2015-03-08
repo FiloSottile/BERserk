78e543ae5fbef9edfc14be77662f753d63a2f2f2
Filippo Valsorda <filippo@cloudflare.com>
2015-03-08 21:52:26 +0000

    Remove RSA1024SHA256DigestInfoTemplate, it was misleading

--- pkcs1v15.go
+++ pkcs1v15.go
@@ -29,25 +29,6 @@ var (
 		},
 		HashLen: 20,
 	}
-	// ### BROKEN
-	// RSA1024SHA256DigestInfoTemplate = &DigestInfoTemplate{
-	// 	BitLen: 1024,
-	// 	Prefix: []byte{
-	// 		0x00, 0x01, 0xFF, 0x00, // PKCS#1 padding
-	// 		0x30, // Tag: SEQUENCE
-	// 		0xC9, // Multi-byte len (0x80 + 0x49)
-	// 	},
-	// 	// ... 0x45 (0xC9 - 0x80 - 4) bytes of garbage ...
-	// 	Suffix: []byte{
-	// 		0x00, 0x00, 0x00, 0x31, // Actual parsed value of multi-byte len
-	// 		0x30, 0x0d, // Tag: SEQUENCE, length: 13
-	// 		0x06, 0x09, // Tag: OID, length: 9
-	// 		0x60, 0x86, 0x48, 0x01, 0x65, 0x03, 0x04, 0x02, 0x01, // SHA256
-	// 		0x05, 0x00, // Tag: NULL, length: 0
-	// 		0x04, 0x20, // Tag: OCTET STRING, length: 32
-	// 	},
-	// 	HashLen: 32,
-	// }
 	RSA2048SHA1DigestInfoTemplate = &DigestInfoTemplate{
 		BitLen: 2048,
 		Prefix: []byte{
